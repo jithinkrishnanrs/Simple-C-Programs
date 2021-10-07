c3d82bf717c59311ad839ea48c458f1ef60dec04
Jithin Krishnan RS <jithinkrishnan42@gmail.com>
2021-10-07 11:28:11 +0530

    odd or even

--- oddoreven.c
+++ oddoreven.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-int main(){
-
-int num;
-printf("Enter a Number: ");
-scanf("%d",&num);
-if (num%2==0){
-   printf("%d is Even",num);
-}
-else{
-   printf("%d is Odd",num);
-}
-return 0;
+int main()
+{
+   int num;
+   printf("Enter a Number: ");
+   scanf("%d", &num);
+   if (num % 2 == 0)
+   {
+      printf("%d is Even", num);
+   }
+   else
+   {
+      printf("%d is Odd", num);
+   }
+   return 0;
 }
